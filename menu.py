52f5b6a21eeb69e86dc18bef9015e05a5ae576e6
AlterWalrus <suraeruarroyo@gmail.com>
2024-10-31 19:22:23 -0600

    separacion de app y menu principal

---
+++ menu.py
@@ -0,0 +1,83 @@
+import ttkbootstrap as ttk
+import tkinter.scrolledtext as st
+from datetime import date
+import serial
+import threading
+
+class MainMenu(ttk.Frame):
+	def __init__(self, parent, controller):
+		ttk.Frame.__init__(self, parent)
+        
+		self.grid_rowconfigure(1, weight=1)
+		self.grid_columnconfigure(1, weight=1)
+        
+		ttk.Label(self, text='SecureRoom', font=('Helvetica', 20, 'bold')).grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky='ew')
+        
+		#Log o historial, como quieras llamarle
+		self.log = st.ScrolledText(self)
+
+		self.log.tag_configure('normal', font=('Consolas', 10))
+		self.log.tag_configure('error', font=('Consolas', 10), foreground='#ff0000')
+		self.log.tag_configure('ok', font=('Consolas', 10), foreground='#00ff00')
+
+		self.log.grid(row=1, column=1, rowspan=8, columnspan=8, padx=10, pady=10, sticky='nsew')
+		self.log.insert(ttk.INSERT, "Bienvenido al sistema de administración SecureRoom.\n", 'normal')
+		self.log.insert(ttk.INSERT, f"Sesión del {date.today()}.\n", 'normal')
+		self.log.configure(state='disabled')
+		
+		#Administracion de tablas
+		fr = ttk.LabelFrame(self, text='Administrar')
+		fr.grid(row=1, column=0, padx=10, pady=1, sticky='n')
+		buttons = ['Médicos', 'Enfermeros', 'Visitantes', 'Pacientes', 'Visitas']
+		for i, button_text in enumerate(buttons):
+			btn = ttk.Button(fr, text=button_text, command=lambda t=button_text: controller.show_frame(t))
+			btn.grid(row=i, column=0, padx=10, pady=10, sticky='ew')
+		
+		#Opciones del log
+		fr = ttk.LabelFrame(self, text='Otras opciones')
+		fr.grid(row=2, column=0, padx=10, pady=10, sticky='ew')
+		ttk.Button(fr, text='Limpiar Log', command=self.log_clear).grid(row=0, column=0, padx=10, pady=10, sticky='ew')
+		ttk.Button(fr, text='Exportar Log', command=self.log_export).grid(row=1, column=0, padx=10, pady=10, sticky='ew')
+
+		#Recibir info del arduino
+		self.arduino = None
+		self.connect_arduino()
+	
+	def connect_arduino(self):
+		try:
+			self.arduino = serial.Serial('COM6', 9600)
+			self.log_print("Conexion establecida correctamente.\n", 'ok')
+			thread = threading.Thread(target=self.read_from_arduino, daemon=True)
+			thread.start()
+		except serial.SerialException as e:
+			self.log_print(f"No se pudo conectar con el lector RFID.\n", 'error')
+			self.log_print(f"Error: {e}\n", 'error')
+
+	def log_print(self, msg, tag='normal'):
+		self.log.configure(state='normal')
+		self.log.insert(ttk.INSERT, msg, tag)
+		self.log.configure(state='disabled')
+	
+	def log_clear(self):
+		self.log.configure(state='normal')
+		self.log.delete('1.0', ttk.END)
+		self.log.configure(state='disabled')
+
+	def log_export(self):
+		try:
+			with open(f'logs/SRlog{date.today()}.txt', 'w', encoding='utf-8') as file:
+				file.write(self.log.get('1.0', ttk.END))
+				self.log_print("Exportado correctamente.", 'ok')
+		except FileNotFoundError as e:
+			self.log_print("Error al exportar.\n", 'error')
+			self.log_print(f"Error: {e}\n", 'error')
+
+	def read_from_arduino(self):
+		try:
+			while True:
+				data = self.arduino.readline().decode('utf-8')
+				if data:
+					self.log_print(data)
+		except serial.SerialException as e:
+			self.log_print(f"Desconexion con el lector RFID.\n", 'error')
+			self.log_print(f"Error: {e}\n", 'error')
