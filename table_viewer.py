b2fa3a39c70bde4682dcf44ce71870fa3592a990
AlterWalrus <suraeruarroyo@gmail.com>
2024-11-07 23:21:48 -0600

    added filter to visits

--- table_viewer.py
+++ table_viewer.py
@@ -16,19 +16,34 @@ class TableViewer(ttk.Frame):
 			'Pacientes': ['CodigoHEX', 'MaxVisitas', 'Habitacion', 'Nombre', 'Apellido P', 'Apellido M', 'Fecha Nac.', 'Telefono', 'N. Casa', 'Calle', 'Colonia', 'CP', 'Email'],
 			'Visitas': ['Entrada', 'Salida', 'Visitante', 'Paciente']
 		}
-		
-		self.corner_frame = ttk.Frame(self)
-		self.corner_frame.grid(row=0, column=0, sticky='w')
-  
-        #Regresar al menu principal
-		ttk.Button(self.corner_frame, width=16, text='Menú anterior', command=lambda: controller.back_to_main(), style='Outline.TButton').grid(row=0, column=0, padx=10, pady=10)
-		ttk.Label(self.corner_frame, text=table_name, anchor='w', font=('Helvetica', 16, 'bold')).grid(row=0, column=1, columnspan=8, padx=10, pady=1, sticky='ew')
+
+		#Filtrar (exclusivo de visitas!!!)
+		if table_name == 'Visitas':
+			fltr = ttk.Frame(self)
+			fltr.grid(row=0, column=1, sticky='e', padx=10)
+
+			ttk.Label(fltr, text='Mostrar visitas de').grid(row=0, column=0, padx=10, pady=5)
+
+			self.filter_menu = ttk.Menubutton(fltr, width=12, text='Todos')
+			self.filter_menu.grid(row=0, column=1, padx=10, pady=5)
+			menu = ttk.Menu(self.filter_menu)
+			for option in ('Todos', 'Médicos', 'Enfermeros', 'Visitantes'):
+				menu.add_radiobutton(label=option, value=option, command=lambda c=option: self.update_filter(c))
+			self.filter_menu['menu'] = menu
+
+		#Regresar al menu principal
+		fr = ttk.Frame(self)
+		fr.grid(row=0, column=0, sticky='w')
+        
+		ttk.Button(fr, width=16, text='Menú anterior', command=lambda: controller.back_to_main(), style='Outline.TButton').grid(row=0, column=0, padx=10, pady=5, sticky='ns')
+		ttk.Label(fr, text=table_name, anchor='w', font=('Helvetica', 16, 'bold')).grid(row=0, column=1, columnspan=8, padx=10, pady=5, sticky='ew')
 
 		#Contenedor de acciones
 		fr_actions = ttk.LabelFrame(self, text='Acciones')
 		fr_actions.grid(row=2, column=0, rowspan=2, padx=10, pady=10, sticky='w')
 		fr_actions.grid_columnconfigure(0, weight=1)
 		fr_actions.grid_columnconfigure(1, weight=1)
+		fr_actions.grid_columnconfigure(2, weight=1)
         
 		#Agregar
 		ttk.Button(fr_actions, width=12, text='Registrar', command=self.open_add).grid(row=0, column=0, padx=10, pady=5)
@@ -73,7 +88,10 @@ class TableViewer(ttk.Frame):
         
 		self.grid_rowconfigure(1, weight=1)
 		self.grid_columnconfigure(0, weight=1)
-    
+	
+	def update_filter(self, content):
+		self.filter_menu['text'] = content
+
 	def update_column_search(self, col):
 		self.search_menu['text'] = col
 
